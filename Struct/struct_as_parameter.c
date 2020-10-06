7098177abfe8299ae98fd40847195c81e9f57ab4
Mucahit KURTLAR <mucahit@tutanota.com>
2020-10-06 13:11:55 +0300

    Add struct as function parameter sample

---
+++ Struct/struct_as_parameter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct student{ //student isminde struct oluşturduk
+  char *name; //bu scope içinde struct üyelerinin sahip olabileceği özllikleri oluşturduk
+  char *id;
+  int age;
+};
+
+void displayStudent(struct student);
+
+int main() {
+  struct student student1; //student1 isimli student tipinde değişken oluşturduk
+  student1.name = "Ali"; // "." ile student1 değişkeninin "name" özelliğine değer atadık
+  student1.id = "19290200"; // "." ile student1 değişkeninin "id" özelliğine değer atadık
+  student1.age = 18; // "." ile student1 değişkeninin "age" özelliğine değer atadık
+  displayStudent(student1);
+  return 0;
+}
+
+void displayStudent(struct student s) {
+    printf("Name: %s\nID: %s\nAge: %d\n", s.name, s.id, s.age);
+}
